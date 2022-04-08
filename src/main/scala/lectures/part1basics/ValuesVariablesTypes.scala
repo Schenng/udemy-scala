8fa78ae2d678f93194bdfa0bc7a1c62aabf9cf48
Simon Cheng <simon@Simons-MacBook-Pro.local>
2022-04-08 18:38:52 -0400

    L1

--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -1,9 +1,24 @@
 package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
-  val x: Int = 1;
 
-  println(x)
+  // Vals are immutable
+  val aInt: Int = 1 // four bytes
+  val aShort: Short = 4613 // two bytes
+  val aLong: Long = 412345212 // eight bytes
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 2.0
 
-  x = 2;
+  val aString: String = "Simon"
+  val aBoolean: Boolean = true
+  val aChar: Char = 'a'
+
+  // Variables (mutable)
+  var aVariable = 4
+  aVariable = 5 // side effects
+
+
+  println(aInt)
+  println(aString)
+  print(aBoolean)
 }
